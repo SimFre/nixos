c024414a32a75cdb2f8d92b9b61430bf197f2a64
Simon Fredriksson <simon@lan2k.org>
2025-07-28 00:11:36 +0200

    wip kodi

--- machines/shlhtpc/configuration.nix
+++ machines/shlhtpc/configuration.nix
@@ -9,7 +9,7 @@
     ./hardware-configuration.nix
     ../../common/common.nix
     ../../common/desktop.nix
-    ../../common/plasma6.nix
+    #../../common/plasma6.nix
   ];
 
   boot = {
@@ -58,8 +58,25 @@
     };
   };
 
-  virtualisation.containers.enable = true;
+  services.xserver.enable = true;
+  services.xserver.desktopManager.kodi.enable = true;
+  services.displayManager.autoLogin.user = "htpc";
+  services.xserver.displayManager.lightdm.greeter.enable = false;
+  users.extraUsers.htpc = {}.isNormalUser = true;
+
+  # Define a user account
+  services.cage.user = "htpc";
+  services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
+  services.cage.enable = true;
+
+  # For Home Manager
+  # home.file.widevine-lib.source = "${pkgs.unfree.widevine-cdm}/share/google/chrome/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so";
+  # home.file.widevine-lib.target = ".kodi/cdm/libwidevinecdm.so";
+  # home.file.widevine-manifest.source = "${pkgs.unfree.widevine-cdm}/share/google/chrome/WidevineCdm/manifest.json";
+  # home.file.widevine-manifest.target = ".kodi/cdm/manifest.json";
+
   virtualisation = {
+    containers.enable = true;
     podman = {
       enable = true;
 
@@ -69,6 +86,17 @@
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
+    oci-containers = {
+      backend = "podman";
+      containers.homeassistant = {
+        volumes = [ "home-assistant:/config" ];
+        environment.TZ = "Europe/Stockholm";
+        image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
+        extraOptions = [ 
+          "--network=host"
+          "--device=/dev/ttyACM0:/dev/ttyACM0"  # Example, change this to match your own hardware
+        ];
+      };
+    };
   };
-
 }
